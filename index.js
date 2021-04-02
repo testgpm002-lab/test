d2f4e5376136d059cbcc0aa85d5ae83c6d6cd656
shahruk khan <https://github.com/testgpm002-lab/test.git>
2021-04-02 17:41:02 +0530

    Test

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const app = require("express")();
 var path = require("path");
-
+const axios = require('axios');
+const { json } = require("express");
 //view engine setup
 app.set("views", path.join(__dirname, "views")); //setting views directory for views.
 app.set("view engine", "hbs"); //setting view engine as handlebars
 
 app.get("/", (req, res) => {
-	let peopleList = getRandomList();
+	let peopleList = loadDoc();
 	res.render("index", { people: peopleList }); //passing list of people to our index.hbs file.
 });
-let getRandomList = () => {
-	let list = ["ada", "turing", "lovelace", "neumann", "gracehopper"];
-	let limit = Math.floor(Math.random() * (list.length - 1 - 0) + 0); //generating random number between 0 & 4
-	return list.slice(limit);
-};
+
+// axios.get('')
+//   .then(function (response) {
+//     // console.log(response)
+    
+//   })
+
+  
+  function getData(){
+    return axios.get('http://127.0.0.1:8000/CityList/')
+  }
+  
+  let peo=Promise.all([getData()]).then(function(result){return result[0]});
+  console.log(peo.then(result)=>{console.log(result)})
+    // response["Access-Control-Allow-Origin"] = "*"
+    // response["Access-Control-Allow-Origin"] = "GET"
+   
+  
+
 app.listen(5000);
